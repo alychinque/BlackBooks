0f0a06a02eb10507458324e3ca06a63a6910a295
alychinque <alychinque@gmail.com>
2021-11-25 03:06:09 +0000

    Created functions to retrieve Readers and to go to Readers screen

--- BlackBooks/src/controller/BlackBooksController.java
+++ BlackBooks/src/controller/BlackBooksController.java
@@ -7,10 +7,14 @@ package controller;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import javax.swing.JOptionPane;
 import model.Book;
+import model.Reader;
 import view.BlackBooks;
 import view.ListBooks;
+import view.Readers;
 
 /**
  *
@@ -20,7 +24,9 @@ public class BlackBooksController {
 
     private final BlackBooks view;
     private ArrayList<Book> library = new ArrayList<>();
+    private Map<String, Reader> readers = new HashMap<>();
     private Book book;
+    private Reader reader;
 
     public BlackBooksController(BlackBooks view) {
         this.view = view;
@@ -38,13 +44,13 @@ public class BlackBooksController {
                     String id = data[0];
                     String authorFirstName = data[1];
                     String authorLastName = data[2];
-                    
+
                     String title = data[3];
                     String bookTitle;
-                    if(title.contains(",")){
-                        String [] split = data[3].split(",");
+                    if (title.contains(",")) {
+                        String[] split = data[3].split(",");
                         bookTitle = split[1].substring(1, split[1].length() - 1) + " " + split[0].substring(1);
-                    }else{
+                    } else {
                         bookTitle = data[3];
                     }
                     String genre = data[4];
@@ -63,9 +69,39 @@ public class BlackBooksController {
     }
 
     public void listAllBooks(ArrayList<Book> library) {
-         int sizeLibrary = library.size();
+        int sizeLibrary = library.size();
         ListBooks lisBooks = new ListBooks(library, sizeLibrary);
         this.view.dispose();
         lisBooks.setVisible(true);
+    }
+
+    public Map<String, Reader> retriveReadersData() {
+        try {
+            BufferedReader csvReader = new BufferedReader(new FileReader("src/Data/READER_DATA.csv"));
+            String row;
+            while ((row = csvReader.readLine()) != null) {
+                String[] data = row.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
+                String idReader = data[0];
+                String readerName = data[1];
+                String readerSurname = data[2];
+                String readerEmail = data[3];
+                String readerAddress = data[4];
+                int phone = Integer.parseInt(data[5]);
+                reader = new Reader(readerName, readerSurname, readerEmail, readerAddress, phone);
+                this.readers.put(idReader, reader);
+            }
+            csvReader.close();
+            return readers;
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Receiving data failed");
+            return null;
+        }
+    }
+
+    public void goReaders(Map<String, Reader> readers) {
+        Readers rd = new Readers(readers);
+        this.view.dispose();
+        rd.setVisible(true);
     }
 }
