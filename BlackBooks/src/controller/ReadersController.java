05efe318c3eed5612392ad6e08d21824b945fc05
alychinque <alychinque@gmail.com>
2021-12-02 02:06:40 +0000

    Added some methods

--- BlackBooks/src/controller/ReadersController.java
+++ BlackBooks/src/controller/ReadersController.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import model.Reader;
 import view.AddReader;
 import view.BlackBooks;
+import view.EditReader;
 import view.ListReaders;
 import view.Readers;
 
@@ -39,5 +40,28 @@ public class ReadersController {
         ListReaders lr = new ListReaders(readers);
         this.view.dispose();
         lr.setVisible(true);
+    }
+    
+    public void goEditReader(int index, ArrayList<Reader> readers) {
+        EditReader er = new EditReader(index, readers);
+        this.view.dispose();
+        er.setVisible(true);
+    }
+
+    public void goEditReader() {
+        EditReader er = new EditReader();
+        this.view.dispose();
+        er.setVisible(true);
+    }
+
+    public int getReader(String id, ArrayList<Reader> readers) {
+        for(int i=0; i< readers.size(); i++){
+            if(readers.get(i).getIdReader().equalsIgnoreCase('"'+id+'"')){
+                System.out.println("returned: "+i);
+                return i;
+            }
+            System.out.println(i);
+        }
+        return -1;
     }
 }
